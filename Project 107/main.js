4b7b1396bb45196479bed8619ba27a34ffb2ca50
Beebo2011 <88395834+Beebo2011@users.noreply.github.com>
2022-09-07 12:46:15 +0200

    Add files via upload

---
+++ Project 107/main.js
@@ -0,0 +1,15 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+var dog = 0;
+var cat = 0;
+
+function gotResults(error, results){
+    
+}
