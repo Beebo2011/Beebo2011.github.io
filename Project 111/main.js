b9f843ded89fe72fd6128735d91c5f5cbfb91150
Beebo2011 <88395834+Beebo2011@users.noreply.github.com>
2022-09-30 16:23:23 +0200

    Add files via upload

---
+++ Project 111/main.js
@@ -0,0 +1,33 @@
+prediction = "";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML = '<img id="image_captured" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/tFCViUOxZ/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded Successfully!");
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    var speak_data = "The Prediction Is "+prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
