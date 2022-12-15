21bf6c53a3a67d9c190e5251ad663b01337542d0
Beebo2011 <88395834+Beebo2011@users.noreply.github.com>
2022-12-15 12:33:48 +0200

    Add files via upload

---
+++ Project 128/main.js
@@ -0,0 +1,44 @@
+Peter_pan_song="";
+Harry_potter_theme_song="";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+
+function preload(){
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+         
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
+}
