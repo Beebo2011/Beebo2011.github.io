aa4bcea9e6e7ef10e81968d167f25b77791f7cfa
Beebo2011 <88395834+Beebo2011@users.noreply.github.com>
2023-01-06 14:32:00 +0200

    Add files via upload

---
+++ Project 132/Fan.js
@@ -0,0 +1,30 @@
+Status = "";
+fan_image = "";
+
+function preload(){
+    fan_image = loadImage("Fan.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(fan_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(fan_image,0,0,640,350);
+}
